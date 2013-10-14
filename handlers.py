683bea3c6a5bde99ba9f59aa7ec630aafe2246ef
Mark Ridgwell <github@markridgwell.com>
2013-10-14 21:38:11 +0100

    Added thumbnail url support

--- handlers.py
+++ handlers.py
@@ -15,10 +15,38 @@ import utils
 
 class IndexHandler(webapp2.RequestHandler):
   def get(self):
-    template_vals = { 'path': self.request.path, 'users' : users }
+    template_vals = { 'path': self.request.path.lower(), 'users' : users }
     self.response.out.write(utils.render_template("index.html", template_vals))
 
+class ResizeImageHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vals = { 'path': self.request.path.lower()[ 6 :], 'users' : users }
+        self.response.out.write(utils.render_template("index.html", template_vals))
+
+class ThumbnailImageHandler(webapp2.RequestHandler):
+    def get(self):
+        template_vals = { 'path': self.request.path.lower()[ 10 : -13 ], 'users' : users }
+        self.response.out.write(utils.render_template("index.html", template_vals))
+
 app = webapp2.WSGIApplication([
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/thumbnail/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/', ResizeImageHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
