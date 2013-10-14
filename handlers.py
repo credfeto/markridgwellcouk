165d7788245b9a3b7ea001ea124d448e52dcd94f
Mark Ridgwell <github@markridgwell.com>
2013-10-14 23:55:43 +0100

    Added image with and hash

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
 import datetime
 import webapp2
 import json
+import hashlib
 from collections import defaultdict 
 
 from google.appengine.api import mail
@@ -14,18 +15,34 @@ import models
 import utils
 
 class IndexHandler(webapp2.RequestHandler):
-  def get(self):
-    template_vals = { 'path': self.request.path.lower(), 'users' : users }
-    self.response.out.write(utils.render_template("index.html", template_vals))
+    def get(self):
+
+        searchPath = self.request.path.lower()
+
+        hash = hashlib.sha512(searchPath).hexdigest()
+
+        template_vals = { 'path': searchPath, 'hash' : hash, 'users' : users }
+        self.response.out.write(utils.render_template("index.html", template_vals))
 
 class ResizeImageHandler(webapp2.RequestHandler):
     def get(self):
-        template_vals = { 'path': self.request.path.lower()[ 6 :], 'users' : users }
+
+        pos = self.request.path.rfind('/') + 1
+        searchPath = self.request.path.lower()[ 6 : pos ]
+        width = self.request.path[ pos : -10 ]
+
+        hash = hashlib.sha512(searchPath).hexdigest()
+
+        template_vals = { 'path': searchPath + ':' + width, 'hash' : hash, 'users' : users }
         self.response.out.write(utils.render_template("index.html", template_vals))
 
 class ThumbnailImageHandler(webapp2.RequestHandler):
     def get(self):
-        template_vals = { 'path': self.request.path.lower()[ 10 : -13 ], 'users' : users }
+        searchPath = self.request.path.lower()[ 10 : -13 ]
+
+        hash = hashlib.sha512(searchPath).hexdigest()
+
+        template_vals = { 'path': searchPath, 'hash' : hash, 'users' : users }
         self.response.out.write(utils.render_template("index.html", template_vals))
 
 app = webapp2.WSGIApplication([
@@ -38,15 +55,15 @@ app = webapp2.WSGIApplication([
     ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
     ('/thumbnail/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
     ('/thumbnail/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/', ResizeImageHandler),
-    ('/image/[\w\-]*/', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/image/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
