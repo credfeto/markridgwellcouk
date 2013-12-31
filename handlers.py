c41e926810a6d3aa75dd4eea31feb2c12713cea9
Mark Ridgwell <github@markridgwell.com>
2013-12-31 19:20:18 +0000

    Used latest jquery and added Social Share Privacy icons

--- handlers.py
+++ handlers.py
@@ -83,50 +83,32 @@ class IndexHandler(webapp2.RequestHandler):
                     resizecss = resizecss + 'wtf'
                 resizecss = resizecss + '</style>'
 
-            template_vals = { 'path': searchPath, 'hash' : hash, 'users' : users, 'title' : item.title, 'item' : item, 'children' : children, 'resizecss' : resizecss }
+            template_vals = { 'path': searchPath, 'hash' : hash, 'users' : users, 'title' : item.title, 'item' : item, 'children' : children, 'resizecss' : resizecss, 'staticurl' : self.request.relative_url('/static') }
             self.response.out.write(utils.render_template("index.html", template_vals))
 
-
-class ResizeImageHandler(webapp2.RequestHandler):
+class LegacyUrlNotFoundNotFoundHandler(webapp2.RequestHandler):
     def get(self):
-
-        pos = self.request.path.rfind('/') + 1
-        searchPath = self.request.path.lower()[ 6 : pos ]
-        width = self.request.path[ pos : -10 ]
-
-        hash = utils.generate_url_hash(searchPath)
-
-        template_vals = { 'path': searchPath + ':' + width, 'hash' : hash, 'users' : users }
-        self.response.out.write(utils.render_template("index.html", template_vals))
-
-class ThumbnailImageHandler(webapp2.RequestHandler):
-    def get(self):
-        searchPath = self.request.path.lower()[ 10 : -13 ]
-
-        hash = utils.generate_url_hash(searchPath)
-
-        template_vals = { 'path': searchPath, 'hash' : hash, 'users' : users }
-        self.response.out.write(utils.render_template("index.html", template_vals))
+        self.response.set_status(404) 
 
 app = webapp2.WSGIApplication([
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/thumbnail/[\w\-]*/thumbnail\.jpg', ThumbnailImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
-    ('/image/[\w\-]*/[0-9]+_image\.jpg', ResizeImageHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/thumbnail/[\w\-]*/thumbnail\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
+    ('/image/[\w\-]*/[0-9]+_image\.jpg', LegacyUrlNotFoundNotFoundHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
     ('/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/[\w\-]*/', IndexHandler),
