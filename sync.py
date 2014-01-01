ec6625a81e9ec95f5d1d69132ff231527e96c7a9
Mark Ridgwell <github@markridgwell.com>
2014-01-01 17:51:40 +0000

    Remote api

---
+++ sync.py
@@ -0,0 +1,142 @@
+import datetime
+import webapp2
+import json
+import hashlib
+import urllib2
+from collections import defaultdict 
+
+from google.appengine.api import mail
+from google.appengine.ext import blobstore
+from google.appengine.api import files
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import users
+from google.appengine.ext import ndb
+from google.appengine.api import urlfetch 
+
+import models
+import utils
+
+def synchronize():
+    url = utils.site_url('/site.js')
+
+    #self.response.write('<br/>Downloading from: ' + url );
+
+    result = urlfetch.fetch(url);
+    if result.status_code == 200:
+        #self.response.write(result.content+ '<br/>')
+
+        decoded = json.loads(result.content)
+        version = decoded["version"]
+
+        self.response.write('<br/>' + "Decoded Version: " + str(version) )
+
+        for item in decoded["items"]:
+            path = item["Path"]
+            title = item["Title"]
+            type = item["Type"]
+            description = item["Description"]
+            rating = None #item["Rating"]
+
+            hash = utils.generate_url_hash(path)
+
+            location = None
+            loc = item["Location"]
+            if loc <> None:
+                if "Latitude" in loc and "Longitude" in loc:
+                    lat = loc["Latitude"]
+                    lng = loc["Latitude"]
+                    location = ndb.GeoPt( lat, lng )
+                
+
+            children = None
+            childItems = item["Children"]
+            if childItems <> None:
+                children = []
+                for child in childItems:
+                        childPath = child["Path"]
+                        childHash = utils.generate_url_hash(childPath)
+                        childTitle = child["Title"]
+                        childType = child["Type"]
+                        childDescription = child["Description"]
+                            
+                        foundThumbnailSize = None
+                        childImageSizes = child["ImageSizes"]
+                        if childImageSizes <> None:
+                            childImageWidth = 0
+                            childImageHeight = 0
+                                
+                            for childImageSize in childImageSizes:
+                                childImageSizeWidth = childImageSize["Width"]
+                                childImageSizeHeight = childImageSize["Height"]
+
+                                if ( childImageWidth == 0 or childImageSizeWidth < childImageWidth) and ( childImageHeight == 0 or childImageSizeHeight < childImageHeight):
+                                    childImageWidth = childImageSizeWidth
+                                    childImageHeight = childImageSizeHeight
+                                    
+                            if childImageWidth <> 0 and childImageHeight <> 0:
+                                foundThumbnailSize = models.ResizedImage(
+                                                                                width = childImageWidth,
+                                                                                height = childImageHeight )
+
+                        children.append( models.ChildItem(
+                                                        id = childHash,
+                                                        path = childPath,
+                                                        title = childTitle,
+                                                        type = childType,
+                                                        description = childDescription,
+                                                        thumbnail = foundThumbnailSize
+                                                    ) )
+
+            #self.response.write('<br/>' + "Path: " + path )
+            #self.response.write('<br/>' + "Hash: " + hash )
+
+            foundImageSizes = None
+            imageSizes = item["ImageSizes"]
+            if imageSizes <> None:
+                foundImageSizes = []
+                for imageSize in imageSizes:                            
+                    imageSizeWidth = imageSize["Width"]
+                    imageSizeHeight = imageSize["Height"]
+                    #self.response.write('<br/>' + " * Size: " + str( imageSizeWidth) + "x" + str( imageSizeHeight) )
+                    foundImageSizes.append( models.ResizedImage(
+                                                                    width = imageSizeWidth,
+                                                                    height = imageSizeHeight
+                                                                ) )
+
+
+                
+
+            q = models.GalleryItem.query(models.GalleryItem.id == hash)
+
+            dbItem = q.get()
+            if dbItem is None:
+                dbItem = models.GalleryItem(
+                                            id = hash,
+                                            path = path,
+                                            title = title,
+                                            type = type,
+                                            description = description,
+                                            rating = rating,
+                                            location = location,
+                                            children = children,
+                                            resizes = foundImageSizes
+                                            )
+                dbItem.put()
+                #self.response.write('<br/>Created')
+            else:
+                dbItem.path = path
+                dbItem.title = title
+                dbItem.type = type
+                dbItem.description = description
+                dbItem.rating = rating
+                dbItem.location = location
+                dbItem.children = children
+                dbItem.resizes = foundImageSizes
+
+                dbItem.put()
+
+                #self.response.write('<br/>Updated')
+
+            #self.response.write('<br/>')
+    #else:
+        #self.response.write("Error!")
---
+++ upload.py
@@ -0,0 +1,11 @@
+from google.appengine.ext.remote_api import remote_api_stub
+import getpass
+import sync
+
+def auth_func():
+  return (raw_input('Username:'), getpass.getpass('Password:'))
+
+remote_api_stub.ConfigureRemoteApi(None, '/_ah/remote_api', auth_func,
+                               'markridgwellcouk.appspot.com')
+
+sync.synchronize()
