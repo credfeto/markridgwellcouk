e3997758b725538bef480a0ba87a5e6e5effdc4a
Mark Ridgwell <github@markridgwell.com>
2014-01-08 21:18:51 +0000

    Added contact and about pages

---
+++ about.py
@@ -0,0 +1,26 @@
+import datetime
+import webapp2
+import json
+from collections import defaultdict 
+from datetime import date
+
+from google.appengine.api import mail
+from google.appengine.ext import blobstore
+from google.appengine.api import files
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import users
+from google.appengine.ext import db
+
+import models
+import utils
+
+
+class AboutHandler(webapp2.RequestHandler):
+    def get(self):
+
+        template_vals = { 'year' : date.today().year + 1 }
+        self.response.out.write(utils.render_template("about.html", template_vals))
+
+app = webapp2.WSGIApplication([
+    ('/about/', AboutHandler),
+])
