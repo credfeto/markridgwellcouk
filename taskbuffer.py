b1a7bbde63c7240897d5e2528e524ac0e34a95eb
Mark Ridgwell <github@markridgwell.com>
2014-05-30 17:30:39 +0100

    Added buffer task

---
+++ taskbuffer.py
@@ -0,0 +1,50 @@
+import datetime
+import webapp2
+import json
+import hashlib
+import urllib2
+from collections import defaultdict 
+
+from google.appengine.api import mail
+from google.appengine.ext import blobstore
+from google.appengine.api import files
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import ndb
+from google.appengine.api import mail
+
+import models
+import utils
+import sync
+import sys
+import pubsubhubub
+
+class TaskBufferHandler(webapp2.RequestHandler):
+    def get(self):
+
+        itemToPublish = models.PublishableItem.fetch(1).get()
+        if itemToPublish <> None:
+
+            publish = models.GalleryItem.query(models.GalleryItem.id == itemToPublish.id ).get()
+            if publish <> None:
+                url = 'http://www.markridgwell.co.uk' + publish.path
+
+                #user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com';
+                user_address = 'mridgwel@hotmail.com'
+
+
+
+                sender_address = "Mark Ridgwell's Photos <bufferpublisher@markridgwellcouk.appspotmail.com>"
+                subject = publish.title + " #photo"
+                body = "@profiles mark ridgwell's photos credfeto @url {" + url
+
+                mail.send_mail(sender_address, user_address, subject, body)
+
+            #itemToPublish.key.delete()
+
+
+        self.response.headers['Content-Type'] = "text/plain"
+        self.response.out.write("OK")
+
+app = webapp2.WSGIApplication([
+    ('/tasks/buffer', TaskSyncHandler)
+])
