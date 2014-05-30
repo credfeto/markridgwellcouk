77140cb9603724c04a8e4492bb550dbde53116cc
Mark Ridgwell <github@markridgwell.com>
2014-05-30 17:44:52 +0100

    Changed to use a loop

--- taskbuffer.py
+++ taskbuffer.py
@@ -13,28 +13,32 @@ import sys
 class TaskBufferHandler(webapp2.RequestHandler):
     def get(self):
 
-        itemToPublish = models.PublishableItem.query().fetch(1).get()
-        if itemToPublish <> None:
-
-            publish = models.GalleryItem.query(models.GalleryItem.id == itemToPublish.id ).get()
-            if publish <> None:
-                url = 'http://www.markridgwell.co.uk' + publish.path
-
-                #user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com';
-                user_address = 'mridgwel@hotmail.com'
-
-
-
-                sender_address = "Mark Ridgwell's Photos <bufferpublisher@markridgwellcouk.appspotmail.com>"
-                subject = publish.title + " #photo"
-                body = "@profiles mark ridgwell's photos credfeto @url {" + url
-
-                mail.send_mail(sender_address, user_address, subject, body)
-
-            #itemToPublish.key.delete()
-
-
         self.response.headers['Content-Type'] = "text/plain"
+        
+        itemsToPublish = models.PublishableItem.query().fetch(1)
+        if itemToPublish <> None:
+            for itemToPublish in itemsToPublish:
+                
+                self.response.out.write("Id: " + itemToPublish.id +"\r\n" )
+
+                publish = models.GalleryItem.query(models.GalleryItem.id == itemToPublish.id ).get()
+                if publish <> None:
+
+                    url = 'http://www.markridgwell.co.uk' + publish.path
+
+                    #user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com';
+                    user_address = 'mridgwel@hotmail.com'
+
+
+
+                    sender_address = "Mark Ridgwell's Photos <bufferpublisher@markridgwellcouk.appspotmail.com>"
+                    subject = publish.title + " #photo"
+                    body = "@profiles mark ridgwell's photos credfeto @url {" + url
+
+                    mail.send_mail(sender_address, user_address, subject, body)
+                    #itemToPublish.key.delete()
+                    break
+
         self.response.out.write("OK")
 
 app = webapp2.WSGIApplication([
