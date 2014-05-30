dd509c65678c1546ddd20a00d69ba9cd6e5c01f1
Mark Ridgwell <github@markridgwell.com>
2014-05-30 18:02:41 +0100

    Killed the item that was being published

--- taskbuffer.py
+++ taskbuffer.py
@@ -24,17 +24,17 @@ class TaskBufferHandler(webapp2.RequestHandler):
                 publish = models.GalleryItem.query(models.GalleryItem.id == itemToPublish.id ).get()
                 if publish <> None:
 
+                    # publish the item
                     url = 'http://www.markridgwell.co.uk' + publish.path
-
-                    user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com';
-                    #user_address = 'markr@markridgwell.co.uk'
-
+                    user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com';                    
                     sender_address = "Mark Ridgwell's Photos <bufferpublisher@markridgwellcouk.appspotmail.com>"
                     subject = publish.title + " #photo"
                     body = "@profiles mark ridgwell's photos credfeto @url " + url
 
                     mail.send_mail(sender_address, user_address, subject, body)
-                    #itemToPublish.key.delete()
+                    
+                    #Remove the item we just published so it doesn't go again
+                    itemToPublish.key.delete()
                     break
 
         self.response.out.write("OK")
