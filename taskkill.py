7ff1563d1b70d820fbfa86a956cac27a785ab46c
Mark Ridgwell <github@markridgwell.com>
2014-07-23 20:48:07 +0100

    Added Kill task

---
+++ taskkill.py
@@ -0,0 +1,32 @@
+import webapp2
+
+from google.appengine.ext import blobstore
+from google.appengine.api import files
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext import ndb
+from google.appengine.api import mail
+
+import models
+import utils
+import sys
+
+class TaskBufferHandler(webapp2.RequestHandler):
+    def get(self):
+
+        self.response.headers['Content-Type'] = "text/plain"
+
+        id = 'cff7bc268029dbf1205927eba4a173aa03797cac03c48409f481be049a8803bef9dab95612cc406d719a25c1ab98fa285fc54199f491da127f5c17f26daa5ac2'
+        
+        publish = models.PublishableItem.query(models.PublishableItem.id == id ).get()
+        if publish <> None:
+
+            
+            #Remove the item 
+            publish.key.delete()
+            break
+
+        self.response.out.write("OK")
+
+app = webapp2.WSGIApplication([
+    ('/tasks/buffer', TaskBufferHandler)
+])
