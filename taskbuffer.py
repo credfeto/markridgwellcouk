8958cf35f9799a440fea250b7a3a1238f62e6ae6
Mark Ridgwell <github@markridgwell.com>
2014-10-05 19:40:47 +0100

    Attempt to push an image with the thing thats posted

--- taskbuffer.py
+++ taskbuffer.py
@@ -6,6 +6,46 @@ import models
 import utils
 
 class TaskBufferHandler(webapp2.RequestHandler):
+
+    def publish_photo(self, files, publish):
+        # publish the item
+        url = 'http://www.markridgwell.co.uk' + publish.path + '?utm_source=mtr&utm_medium=buffer&utm_campaign=publish'
+        shortened_url = url
+        user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com'
+        sender_address = "Mark Ridgwell's Photos <bufferpublisher@markridgwellcouk.appspotmail.com>"
+        subject = publish.title + " #photo " + shortened_url
+        body = "@profiles mark ridgwell's photos credfeto " \
+               "@now " \
+               "@url " + url
+        if files is None:
+            mail.send_mail(sender=sender_address, to=user_address, subject=subject, body=body)
+        else:
+            mail.send_mail(sender=sender_address, to=user_address, subject=subject, body=body,
+                           attachments=files)
+
+    def get_resize(self, publish):
+        ordered_resizes = sorted(publish.resizes, key=lambda r: r.width)
+        image = ordered_resizes[-1]
+        for (i, resize ) in enumerate(ordered_resizes):
+            if resize.width <= 800:
+                image = resize
+
+        return image
+
+    def fetch_image_to_attach(self, image, publish):
+        image_url = utils.image_url(publish.path, image)
+        self.response.out.write("Image: " + image_url + "\r\n")
+        files = None
+        result = urlfetch.fetch(image_url)
+        self.response.out.write("Status: " + str(result.status_code) + "\r\n")
+        if result.status_code == 200:
+            filename = publish.title + ".jpg"
+            file_data = result.content
+            files = [(filename, file_data)]
+
+            self.response.out.write("Adding Attachment: " + len(file_data) + "\r\n")
+        return files
+
     def get(self):
 
         utils.add_response_headers( self.request, self.response.headers )
@@ -20,43 +60,12 @@ class TaskBufferHandler(webapp2.RequestHandler):
                 publish = models.GalleryItem.query(models.GalleryItem.id == itemToPublish.id ).get()
                 if publish is not None:
 
-                    ordered_resizes = sorted(publish.resizes, key=lambda r: r.width)
-                    image = ordered_resizes[-1]
-                    for (i, resize ) in enumerate(ordered_resizes):
-                        if resize.width <= 800:
-                            image = resize
-                            break
+                    image = self.get_resize(publish)
 
-                    image_url = utils.image_url(publish.path, image)
-                    self.response.out.write("Image: " + image_url +"\r\n" )
+                    files = self.fetch_image_to_attach(image, publish)
 
+                    self.publish_photo(files, publish)
 
-                    files = None
-                    result = urlfetch.urlopen(image_url)
-                    self.response.out.write("Status: " + str(result.status_code) +"\r\n" )
-                    if result.status_code == 200:
-                        filename = publish.title + ".jpg"
-                        file_data = result.content
-                        files = [(filename, file_data)]
-
-                        self.response.out.write("Adding Attachment: " + len(file_data) +"\r\n" )
-
-                    # publish the item
-                    url = 'http://www.markridgwell.co.uk' + publish.path + '?utm_source=mtr&utm_medium=buffer&utm_campaign=publish'
-                    shortened_url = url
-                    user_address = 'buffer-62c71f8f12deed183390@to.bufferapp.com'
-                    sender_address = "Mark Ridgwell's Photos <bufferpublisher@markridgwellcouk.appspotmail.com>"
-                    subject = publish.title + " #photo " + shortened_url
-                    body = "@profiles mark ridgwell's photos credfeto " \
-                           "@now " \
-                           "@url " + url
-
-                    if files is None:
-                        mail.send_mail(sender=sender_address, to=user_address, subject=subject, body=body)
-                    else:
-                        mail.send_mail(sender=sender_address, to=user_address, subject=subject, body=body,
-                                       attachments=files)
-                    
                     #Remove the item we just published so it doesn't go again
                     # itemToPublish.key.delete()
                     break
