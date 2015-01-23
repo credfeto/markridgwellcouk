7b6a89d804bfc5765f2868f061dbb79e288c511d
Mark Ridgwell <github@markridgwell.com>
2015-01-23 23:10:32 +0000

    Basic XMPP

--- taskkill.py
+++ taskkill.py
@@ -9,6 +9,7 @@ from google.appengine.api import mail
 import models
 import utils
 import sys
+from google.appengine.api import xmpp
 
 
 class TaskKillHandler(webapp2.RequestHandler):
@@ -19,6 +20,8 @@ class TaskKillHandler(webapp2.RequestHandler):
 
         id = 'cff7bc268029dbf1205927eba4a173aa03797cac03c48409f481be049a8803bef9dab95612cc406d719a25c1ab98fa285fc54199f491da127f5c17f26daa5ac2'
 
+        status = ''
+
         publish = models.PublishableItem.query(models.PublishableItem.id == id).get()
         if publish <> None:
 
@@ -26,9 +29,21 @@ class TaskKillHandler(webapp2.RequestHandler):
             # Remove the item
             publish.key.delete()
 
+            status = 'Deleted'
             self.response.out.write("Deleted")
         else:
-            self.response.out.write("Not Found")
+            status = 'Not Found'
+
+        user_address = 'markr@markridgwell.com'
+        #chat_message_sent = False
+        msg = "Deletion status: " + status
+        xmpp.send_message(user_address, msg)
+        #status_code = xmpp.send_message(user_address, msg)
+        #chat_message_sent = (status_code == xmpp.NO_ERROR)
+
+        #if not chat_message_sent:
+
+        self.response.out.write(status)
 
 
 app = webapp2.WSGIApplication([
