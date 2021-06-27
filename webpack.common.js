72c2f5c9a2544a3fd800588aa42c426911fd5a36
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-06-27 01:11:07 +0100

    Fixed analysis issues

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const RobotstxtPlugin = require("robotstxt-webpack-plugin");
 const ImageminWebpWebpackPlugin= require("imagemin-webp-webpack-plugin");
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,5 +6,5 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
-  },
+  }
 });
